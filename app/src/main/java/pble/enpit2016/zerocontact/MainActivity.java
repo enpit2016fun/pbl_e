83b2aa17bd31bd23a28004e25cfb91b250330311
TakayaYAMADA <16043065@mmm.muroran-it.ac.jp>
2016-11-04 18:46:12 +0900

    'コメント「確認」を追加'

--- app/src/main/java/pble/enpit2016/zerocontact/MainActivity.java
+++ app/src/main/java/pble/enpit2016/zerocontact/MainActivity.java
@@ -24,6 +24,7 @@ import pble.enpit2016.zerocontact.parts.PagerAdapter;
 /**
  * 主な機能のページ遷移を管理するアクティビティ
  * Created by kyokn on 2016/10/31.
+ * 確認　山田
  */
 
 public class MainActivity extends AppCompatActivity
