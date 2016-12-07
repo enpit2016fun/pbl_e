fcc8557a957682cc07c46527800de143aed03b95
Kazama Yoshimitsu <g2116009@fun.ac.jp>
2016-12-07 14:41:56 +0900

    編集用に変えていた画面遷移を元に戻した．

--- app/src/main/java/pble/enpit2016/zerocontact/parts/PagerAdapter.java
+++ app/src/main/java/pble/enpit2016/zerocontact/parts/PagerAdapter.java
@@ -35,7 +35,7 @@ public class PagerAdapter extends FragmentPagerAdapter implements CustomViewPage
             case 1:
                 return FavoriteFragment.newInstance();
             case 2:
-                return ProfileEditFragment.newInstance();
+                return ProfileFragment.newInstance();
             default:
                 return TestFragment.newInstance(position);
         }
